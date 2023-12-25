69f5c9a491483de4a1f6405e31b86cea4cc3ac38
EdwinLolo <edwinfedora@gmail.com>
2023-12-26 00:17:44 +0700

    page4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Cover from "./components/cover/cover";
 import Page1 from "./components/page1_1/page1_1";
 import Page3 from "./components/page3/page3";
+import Page4 from "./components/page4/page4";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Cover />
       <Page1 />
       <Page3 />
+      <Page4 />
     </div>
   );
 }
---
+++ src/components/page4/page4.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./stylepage4.css";
+
+function page4() {
+  return (
+    <div className="boxxess">
+      <div className="kiri">
+        <div className="kiriatas"></div>
+        <div className="kiribawah"></div>
+      </div>
+      <div className="kanan">
+        <div className="kananatas"></div>
+        <div className="kananbawah"></div>
+      </div>
+    </div>
+  );
+}
+
+export default page4;
