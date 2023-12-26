d9f9e5c9348b4ef9f2befc9a22471d1db1ca1c43
EdwinLolo <edwinfedora@gmail.com>
2023-12-26 14:28:16 +0700

    page4

--- src/components/page4/page4.js
+++ src/components/page4/page4.js
@@ -5,11 +5,63 @@ function page4() {
   return (
     <div className="boxxess">
       <div className="kiri">
-        <div className="kiriatas"></div>
-        <div className="kiribawah"></div>
+        <div className="kiriatas">
+          <h1>
+            Indonesia dikenal dengan <br /> keragaman dan keunikan <br />{" "}
+            budayanya.
+          </h1>
+          <p>
+            Buktinya, Kementerian Pendidikan, Kebudayaan, Riset, dan <br />{" "}
+            Teknologi mencatat terdapat 1.728 Warisan Budaya Takbenda <br />{" "}
+            (WBTb) di Indonesia pada periode 2013 sampai 2022.
+          </p>
+        </div>
+        <div className="kiribawah row"></div>
       </div>
       <div className="kanan">
-        <div className="kananatas"></div>
+        <div className="kananatas row">
+          <h5>Jumlah tersebut terdiri dari</h5>
+          <div className="col-6">
+            <h1>491</h1>
+            <p>
+              Warisan budaya dalam <br /> domain Adat Istiadat <br />{" "}
+              Masyarakat, Ritus, dan <br />
+              Perayaan-Perayaan
+            </p>
+          </div>
+          <div className="col-6">
+            <h1>491</h1>
+            <p>
+              Warisan budaya dalam <br /> domain Adat Istiadat <br />{" "}
+              Masyarakat, Ritus, dan <br />
+              Perayaan-Perayaan
+            </p>
+          </div>
+          <div className="col-6">
+            <h1>491</h1>
+            <p>
+              Warisan budaya dalam <br /> domain Adat Istiadat <br />{" "}
+              Masyarakat, Ritus, dan <br />
+              Perayaan-Perayaan
+            </p>
+          </div>
+          <div className="col-6">
+            <h1>491</h1>
+            <p>
+              Warisan budaya dalam <br /> domain Adat Istiadat <br />{" "}
+              Masyarakat, Ritus, dan <br />
+              Perayaan-Perayaan
+            </p>
+          </div>
+          <div className="col-6">
+            <h1>491</h1>
+            <p>
+              Warisan budaya dalam <br /> domain Adat Istiadat <br />{" "}
+              Masyarakat, Ritus, dan <br />
+              Perayaan-Perayaan
+            </p>
+          </div>
+        </div>
         <div className="kananbawah"></div>
       </div>
     </div>
